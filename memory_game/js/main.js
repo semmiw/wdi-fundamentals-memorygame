f4b47827af46bd9c2f3d3146223aff35a9b8542d
Semhar Woldeyesus <semmiw@Semhars-MacBook-Air.local>
2018-11-21 16:23:37 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('queen');
-console.log("User flipped queen"); 
-var cardTwo = cards[3];
-cardsInPlay.push('king');
-console.log("User flipped king");
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else { 
-	alert("Sorry, try again.");
-}
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+};
+var flipCard = function (cardId) {
+	console.log("User flipped" + cards[cardId])
+};
+var errorAlert = function() {
+  alert("Please be sure to fill out all required fields.");
+};
+
+cards[cardId].push(cardsInPlay);
+flipCard();
+flipCard(0);
+flipCard(2);
