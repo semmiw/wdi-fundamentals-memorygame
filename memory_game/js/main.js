a8c459418c3504c02889a8c03006a406045775c7
Semhar Woldeyesus <semmiw@Semhars-MacBook-Air.local>
2018-11-21 01:03:42 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,13 @@
-console.log("Up and running!")
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('queen');
+console.log("User flipped queen"); 
+var cardTwo = cards[3];
+cardsInPlay.push('king');
+console.log("User flipped king");
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else { 
+	alert("Sorry, try again.");
+}
