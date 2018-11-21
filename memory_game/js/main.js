d85da87937c7dd5b8cd99fb0b547f75121530d96
Semhar Woldeyesus <semmiw@Semhars-MacBook-Air.local>
2018-11-21 18:52:14 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"	
+}
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",	
+}
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"	
+}
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -7,13 +28,15 @@ console.log("You found a match!");
 console.log("Sorry, try again.");
 };
 var flipCard = function (cardId) {
-	console.log("User flipped" + cards[cardId])
-};
+	console.log("User flipped" + cards[cardId].rank);
+}
 var errorAlert = function() {
   alert("Please be sure to fill out all required fields.");
 };
 
-cards[cardId].push(cardsInPlay);
+cardsInPlay.push(cards[rank]);
 flipCard();
 flipCard(0);
 flipCard(2);
+console.log(cardImage)
+console.log(suit)
