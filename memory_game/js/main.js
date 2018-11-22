1db65d403b62a0e317be8efae1819bfc8dbf556c
Semhar Woldeyesus <semmiw@Semhars-MacBook-Air.local>
2018-11-22 12:29:01 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,12 +22,13 @@ cardImage: "images/king-of-diamonds.png"
 ];
 var cardsInPlay = [];
 var checkForMatch = function () {
+	this.setAttribute(cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 console.log("You found a match!");
 } else {
 console.log("Sorry, try again.");
 };
-var flipCard = function (cardId) {
+var flipCard = function (this.getAttribute(cards[cardId]); {
 	console.log("User flipped" + cards[cardId].rank);
 }
 var errorAlert = function() {
@@ -35,8 +36,17 @@ var errorAlert = function() {
 };
 
 cardsInPlay.push(cards[rank]);
-flipCard();
-flipCard(0);
-flipCard(2);
 console.log(cardImage)
 console.log(suit)
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+    // loop through the cards array
+var cardElement = document.createElement('img');
+cardElement.setAttribute(data-i, 'images/back.png');
+cardElement.addEventListener('click', flipCard);
+var node = document.createElement ("game-board")
+var textnode = document.createTextNode(cardElement); 
+node.appendChild(textnode);
+document.getElementById("game-board").appendChild(node);     // Append <li> to <ul> with id="myList"
+};
+var createBoard = function ();
